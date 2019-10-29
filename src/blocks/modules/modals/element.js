42583b3bbfca02edcb19480a5d04555860117f51
Sasha Zayets <sachazaets@ukr.net>
2019-10-29 22:36:59 +0200

    new functionality added

--- src/blocks/modules/modals/element.js
+++ src/blocks/modules/modals/element.js
@@ -1,4 +1,8 @@
 export default class Elements {
+    init (options) {
+        this.options = Object.assign({}, options);
+    }
+
     searchElement (selector) {
         const element = document.querySelector(selector);
 
@@ -7,5 +11,47 @@ export default class Elements {
         }	else {
             return null;
         }
+    }
+  
+    сreateElement (nameTag) {
+        return document.createElement(nameTag);
+    }
+  
+    addTextElement (element, text) {
+      element.innerHTML = text;
+    }
+  
+    addClassElement (element, nameClass) {
+      element.classList.add(nameClass)
+    }
+  
+    appendElement (element, appendBlock) {
+        appendBlock.append(element)
+    }
+  
+    generateBottomBlock () {
+      const bottomBlock = this.сreateElement('div');
+      const btnClose = this.сreateElement('button');
+      const btnConfirm = this.сreateElement('button');
+      
+      this.addTextElement(btnClose, this.options.cancelBtnText);
+      this.addClassElement(btnClose, this.options.classCancelBtn);
+      
+      this.addTextElement(btnConfirm, this.options.confirmBtnText);
+      this.addClassElement(btnConfirm, this.options.classConfirmBtn);
+      
+      this.appendElement(btnClose, bottomBlock);
+      this.appendElement(btnConfirm, bottomBlock);
+      this.addClassElement(bottomBlock, 'modals-bottom');
+      
+      return bottomBlock;
+    }
+
+    generateHeadBlock () {
+        const headerBlock = this.сreateElement('div');
+        this.addClassElement(headerBlock, 'modals-header');
+        this.addTextElement(headerBlock, this.options.headerTitle);
+
+        return headerBlock;
     }
 }
--- src/blocks/modules/modals/modals.js
+++ src/blocks/modules/modals/modals.js
@@ -4,22 +4,47 @@ import Elements from './element';
 const defaultOptions = Object.freeze({
     btnClass: 'btn-modal',
     modalClass: 'modals',
-    animation: 'fade'
+    animation: 'scale',
+    bottomBlock: false,
+    cancelBtnText: 'cancel',
+    confirmBtnText: 'confirm',
+    classCancelBtn: 'cancel',
+    classConfirmBtn: 'confirm',
+    headerModal: true,
+    headerTitle: 'Title modals',
 })
 
 class Modal extends Elements {
     constructor (options) {
         super();
-        this.options = {};
+        this.options = {}
 
         let mergedOptions = Object.assign({}, defaultOptions);
         this.options = Object.assign(mergedOptions, options);
-
+       
         this.btnClickSelector = this.options.btnClass;
         this.modalSelector = this.options.modalClass;
         this.nameAnimation = this.options.animation;
 
         this.init();
+
+        this.modalContainer = this.modals.firstElementChild;
+      
+        this.optionsElemets = {
+            cancelBtnText: this.options.cancelBtnText,
+            confirmBtnText: this.options.confirmBtnText,
+            classCancelBtn: this.options.classCancelBtn,
+            classConfirmBtn: this.options.classConfirmBtn,
+            headerTitle: this.options.headerTitle
+        }
+
+        if (this.options.bottomBlock) {
+          this.addBottomBlock();
+        }
+
+        if (this.options.headerModal) {
+            this.addHeaderModal();
+        }
     }
 
     init () {
@@ -31,12 +56,19 @@ class Modal extends Elements {
 
     open () {
         const iconClose = document.querySelector('.modals-close');
-
+        
         this.modals.classList.add('show');
-        this.closeModal(this.modals, iconClose);
+
+        if (this.options.bottomBlock) {
+            const closeBtn = document.querySelector('.modals .close');
+          
+            this.closeModal(this.modals, iconClose, closeBtn);
+        } else {
+          this.closeModal(this.modals, iconClose);  
+        }
     }
 
-   addEvent () {
+    addEvent () {
         const btn = super.searchElement(`.${this.btnClickSelector}`);
         
         btn.addEventListener('click', (event) => {
@@ -45,20 +77,17 @@ class Modal extends Elements {
     }
 
     closeModal (...eventElement) {
-        const modalContainer = this.modals.firstElementChild;
-
         eventElement.forEach(element => {
             element.addEventListener('click', (event) => {
-                if (event.target.contains(modalContainer) && !event.target.classList.contains('modals-container')) {
-                    if (!modalContainer.contains(event.target)) {
+                if (event.target.contains(this.modalContainer) && !event.target.classList.contains('modals-container')) {
+                    if (!this.modalContainer.contains(event.target)) {
                         this.fadeModal();
                     }
-                } else if (event.target.classList.contains('modals-close')) {
+                } else if (event.target.classList.contains('modals-close') || event.target.classList.contains('close')) {
                     this.fadeModal();
                 }
             });
         });
-        
     }
 
     fadeModal  () {
@@ -67,6 +96,20 @@ class Modal extends Elements {
 
     addAnimation () {
         this.modals.classList.add(this.nameAnimation);
+    }
+
+    addHeaderModal () {
+        super.init(this.optionsElemets);
+
+        const header = super.generateHeadBlock();
+        this.modalContainer.prepend(header);
+    }
+
+    addBottomBlock () {
+        super.init(this.optionsElemets);
+
+        const bottom = super.generateBottomBlock();
+        this.modalContainer.append(bottom);
     }
 }
 
